bd29c7b5e0d8827d4de181472f938e46ce9ce28a
creative-kaif <syedkaift@gmail.com>
2025-09-14 23:58:45 +0530

    sdfdsf

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Pricing from "./components/Pricing.jsx";
 import Download from "./components/Download.jsx";
 import Cta from "./components/CTA.jsx";
 import Footer from "./components/footer.jsx";
-import {motion} from "motion/react";
-import Banner from "../src/assets/img/banner-2.png";
+import { motion } from "motion/react";
+import Banner from "../src/assets/img/banner-2.jpg";
 
 
 const App = () => {
@@ -16,23 +16,23 @@ const App = () => {
 
             <div className="overflow-x-hidden img absolute w-fit max-md:top-0 top-0 h-screen -z-50 ">
                 <motion.img
-                    initial={{opacity: 0, scale: 0.9}}
-                    animate={{opacity: 1, scale: 1}}
-                    transition={{duration: 1.4, delay: 0.4}}
+                    initial={{ opacity: 0, scale: 0.9 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 1.4, delay: 0.4 }}
 
                     src={Banner} className="w-100% bg-cover object-cover fixed z-0"
-                    alt=""/>
+                    alt="" />
 
 
 
             </div>
-            <Navbar/>
-            <Hero/>
-            <Why/>
-            <Pricing/>
-            <Download/>
-            <Cta/>
-            <Footer/>
+            <Navbar />
+            <Hero />
+            <Why />
+            <Pricing />
+            <Download />
+            <Cta />
+            <Footer />
         </main>
     )
 }
