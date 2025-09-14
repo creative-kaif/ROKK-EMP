f0c4825440c330a0123d97ac0c0c69b3bdfbd4b4
creative-kaif <syedkaift@gmail.com>
2025-09-14 23:52:31 +0530

    BSC Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
 
                         <div className="flex items-center justify-center">
                             <p className="flex items-center relative mt-12 max-md:mt-8 max-md:text-sm font-Poppins-Regular text-white text-lg text-center">Exklusiv für <img
-                                src={TFLogo} className="w-32 p-2" alt="" />BSC Mitglieder</p>
+                                src={TFLogo} className="w-32 p-2" alt="" />Backstage Club Mitglieder</p>
 
                         </div>
                     </motion.div>
